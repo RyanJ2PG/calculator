ea668265ca407064e7eb669cca19534f1c821338
Ryan Herbert <RyanJ2PG@gmail.com>
2021-06-30 13:08:00 -0400

    created and got the 1st array (num1Arr) to work as intended

--- main.js
+++ main.js
@@ -5,18 +5,20 @@
 // create a function that takes an operator and 2 numbers and calls the function on those nums
 let btnContainer = document.querySelector('#btnContainer');
 console.log(btnContainer);
-let num1 = 0; // will equal num1Arr
-let num2 = 0; // will equal num2Arr
+let num1 = ""; // will equal num1Arr
+let num2 = ""; // will equal num2Arr
+let hasOperated = false;
 let operator = "";
 let num1Arr = [];
 let num2Arr = [];
-let num1Join = num1Arr.join('');
 let isClicked = false;
-num1 = parseInt(prompt("What is the first input value?"));
-num2 = parseInt(prompt("What is the second input value?"));
+// num1 = parseInt(prompt("What is the first input value?"));
+// num2 = parseInt(prompt("What is the second input value?"));
 
 function Add(x, y){
     let sumAdd = x + y;
+    console.log(x);
+    console.log(y);
     // num1 = sumAdd;
     return sumAdd;
 }
@@ -41,20 +43,26 @@ function Divide(x, y){
 }
 
 function Operate(){
+    hasOperated = true;
     if(operator === "+"){
+        // if (num2 !== ""){
+        // }
+        num1ArrJoin();
+        num1Arr = [];
+        // else {
+        // num2ArrJoin();
         num1 = Add(num1, num2);
-        console.log(num1);
+        // num2 = "";
+        // }
     }
     else if (operator === "-"){
         num1 = Subtract(num1, num2);
-        console.log(num1);
 
     }
     // else if (operator === "÷"){
     //     num1 = Divide(num1, num2);
     // }
-    // else if (operator === "×"){
-    //     num1 = Multiply(num1, num2);
+ 
     // }
     // else if (operator === "="){
         
@@ -82,7 +90,7 @@ function CreateBtn(name, btnOp, number){
         if(btnOp !== null){
             operator = btnOp;
             Operate();
-        }else if (number !== null){
+        }else if (number){
             num1Arr.push(name);
             console.log(num1Arr);
         }else if (name === "AC"){
@@ -94,8 +102,22 @@ function CreateBtn(name, btnOp, number){
     });
 }
 
+function num1ArrJoin(){
+    for (let i = 0; i < num1Arr.length; i++){
+        num1 = num1 + num1Arr[i];
+    }
+    num1 = parseInt(num1);
+}
+
+function num2ArrJoin(){
+    for (let i = 0; i < num2Arr.length; i++){
+        num2 = num2 + num2Arr[i];
+    }
+    num2 = parseInt(num2);
+}
+
 for(i = 0; i <= 9; i++){
-    CreateBtn(i.toString(), null, );
+    CreateBtn(i.toString(), null, true);
 }
 CreateBtn(".", null, null); //not done yet
 CreateBtn("C", null, null);
