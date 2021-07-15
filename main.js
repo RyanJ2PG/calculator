362d6553f5d2c27154fa1485bc391f560ec31426
Ryan Herbert <RyanJ2PG@gmail.com>
2021-07-15 16:17:37 -0400

    fixed the calculator input text field and output field, and the AC and C buttons

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 // each operator function MUST save the sum of the first two inputs as ---
 // a variable and add each input after to that variable and save it again
 // create a function that takes an operator and 2 numbers and calls the function on those nums
+let calcOutput = document.querySelector("#result");
+let calcInput = document.querySelector("#input")
 let btnContainer = document.querySelector('#btnContainer');
 let num1 = ""; // will equal num1Arr
 let num2 = ""; // will equal num2Arr
@@ -11,7 +13,7 @@ let operator = "";
 let previousOperator = null;
 let num1Arr = [];
 let num2Arr = [];
-let isClicked = false;
+let decimalPressed = false;
 
 function Add(x, y){
     let sumAdd = x + y;
@@ -30,80 +32,73 @@ function Multiply(x, y){
 
 function Divide(x, y){
     let sumDivide = (x / y);
-    let sumDivideRounded = Math.round(100000000 * sumDivide) / 100000000;
+    let sumDivideRounded = Math.round(1000000 * sumDivide) / 1000000;
     return sumDivideRounded;
 }
 
 function Operate(){
+    decimalPressed = false;
+    num1ArrJoin();
+    num2ArrJoin();
+    num1Arr = [];
+    num2Arr = [];
+    if (operator === "=" && num2 === ""){
+        return;
+    }
+    calcInput.innerHTML = "";
     hasOperated = true;
     tmpOperator = operator; // tmpOp = null , op is + || tmpOp = null , op is - || tmpOp = - 
     console.log("this is prev op "+ previousOperator)
     if (previousOperator !== operator){ // previousOp is null, op is + || previousOp is +, op - ||
         tmpOperator = previousOperator; // null = null || tmp + previous + ||
         previousOperator = operator; // + = + || prebious - = op - ||
-        console.log("this is prev op "+ previousOperator)
+        console.log("this is prev op "+ previousOperator)   
     }
     if(tmpOperator === "+"){
-        num1ArrJoin();
-        num2ArrJoin();
-        num1Arr = [];
-        num2Arr = [];
         if (num2 !== ""){
             num1 = Add(num1, num2);
             console.log("equals " + num1);
+            calcOutput.innerHTML = num1;
             num2 = "";
         }
     }
     else if (tmpOperator === "-"){
-        num1ArrJoin();
-        num2ArrJoin();
-        num1Arr = [];
-        num2Arr = [];
         if (num2 !== ""){
             num1 = Subtract(num1, num2);
             console.log("equals " + num1);
+            calcOutput.innerHTML = num1;
             num2 = "";
         }
-
     }
     else if (tmpOperator === "/"){
-        num1ArrJoin();
-        num2ArrJoin();
-        num1Arr = [];
-        num2Arr = [];
         if (num2 !== ""){
             num1 = Divide(num1, num2);
             console.log("equals " + num1);
+            calcOutput.innerHTML = num1;
             num2 = "";
         }
     }
     else if (tmpOperator === "*"){
-        num1ArrJoin();
-        num2ArrJoin();
-        num1Arr = [];
-        num2Arr = [];
         if (num2 !== ""){
             num1 = Multiply(num1, num2);
             console.log("equals " + num1);
+            calcOutput.innerHTML = num1;
             num2 = "";
         }
     }
-    else if (tmpOperator === "EQ"){
-        num1ArrJoin();
-        num2ArrJoin();
-        num1Arr = [];
-        num2Arr = [];
-        num2 = "";         
+    else if (tmpOperator === "=" && num2 !== ""){
+        num2 = "";
+        calcOutput.innerHTML = num1;
     }
 };
-
 //Creates each button and assigns the name of the operator and number on click
-function CreateBtn(name, btnOp, number, className){
+function CreateBtn(name, btnOp, number, idName){
     let btn = document.createElement('button');
     let btnText = document.createTextNode(name);
     btn.appendChild(btnText);
     btnContainer.appendChild(btn);
-    btn.setAttribute('id', className);
+    btn.setAttribute('id', idName);
+    btn.classList.add("btn");
     btn.addEventListener('click', function(){
         if (hasOperated === false){
             if(btnOp !== null){
@@ -111,25 +106,55 @@ function CreateBtn(name, btnOp, number, className){
                 console.log("this is the operator = " + operator);
                 Operate();
             }else if (number){
-                num1Arr.push(name);
+                if (name === "." && decimalPressed === false){
+                    decimalPressed = true;
+                    num1Arr.push(name);
+                    calcInput.innerHTML = calcInput.innerHTML + name;
+                } else if (name !== "."){
+                    num1Arr.push(name);
+                    calcInput.innerHTML = calcInput.innerHTML + name;
+                }
                 console.log("this is num1Arr " + num1Arr);
             }else if (name === "AC"){
                 num1 = "";
                 num2 = "";
-            } else if (name === "C")
+                num1Arr = [];
+                num2Arr = [];
+                hasOperated = false;
+                calcOutput.innerHTML = "";
+                calcInput.innerHTML = "";
+                decimalPressed = false;
+            } else if (name === "C"){
                 num1Arr.pop();
+                calcInput.innerHTML = calcInput.innerHTML.slice(0, -1);
+            }
         } else if (hasOperated === true){
             if(btnOp !== null){
                 operator = btnOp;
                 Operate();
             }else if (number){
-                num2Arr.push(name);
+                if (name === "." && decimalPressed === false){
+                    decimalPressed = true;
+                    num2Arr.push(name);
+                    calcInput.innerHTML = calcInput.innerHTML + name;
+                } else if (name !== "."){
+                    num2Arr.push(name);
+                    calcInput.innerHTML = calcInput.innerHTML + name;
+                }
                 console.log("this is num2Arr " + num2Arr);
             }else if (name === "AC"){ 
                 num1 = "";
                 num2 = "";
-            } else if (name === "C")
+                num1Arr = [];
+                num2Arr = [];
+                hasOperated = false;
+                calcOutput.innerHTML = "";
+                calcInput.innerHTML = "";
+                decimalPressed = false;
+            } else if (name === "C"){
                 num2Arr.pop();
+                calcInput.innerHTML = calcInput.innerHTML.slice(0, -1);
+            }
         }
     });
 }
@@ -152,6 +177,12 @@ function num2ArrJoin(){
     }
 }
 
+function disableDec(){
+    if (calcInput.innerHTML.includes(".")){
+        document.getElementById("decimal").disabled = true;
+    }
+}
+
 CreateBtn("🧑‍🏫", "=", null, "emoji");
 CreateBtn("AC", null, null, "AC");
 CreateBtn("C", null, null, "C");
@@ -171,6 +202,8 @@ CreateBtn("-", "-", null, "subtract");
 CreateBtn("0", null, true, "zero");
 CreateBtn(".", null, true, "decimal");
 CreateBtn("=", "=", null, "equals");
+let calcBtns = document.querySelectorAll(".btn");
+
 
 
 
